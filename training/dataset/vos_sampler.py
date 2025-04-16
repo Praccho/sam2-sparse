b61ad1e702e63dbf60bbc26e8e5c79a781833a38
Praccho <praccho@brown.edu>
2025-04-16 15:45:54 -0400

    sparse sampler

--- training/dataset/vos_sampler.py
+++ training/dataset/vos_sampler.py
@@ -77,6 +77,55 @@ class RandomUniformSampler(VOSSampler):
         )
         return SampledFramesAndObjects(frames=frames, object_ids=object_ids)
 
+class SparseSampler(VOSSampler):
+    def __init__(
+        self,
+        num_frames,
+        max_num_objects,
+        random_permute=False,
+    ):
+        self.num_frames = num_frames
+        self.max_num_objects = max_num_objects
+        self.random_permute = random_permute
+
+    def sample(self, video, segment_loader, epoch=None):
+
+        for retry in range(MAX_RETRIES):
+            if len(video.frames) < self.num_frames:
+                raise Exception(
+                    f"Cannot sample {self.num_frames} frames from video {video.video_name} as it only has {len(video.frames)} annotated frames."
+                )
+            
+            step_sz = (len(video.frames)-1) / (self.num_frames-1) if self.num_frames != 1 and len(video.frames) != 1 else 0
+            frames = [video.frames[round(step * step_sz)] for step in range(self.num_frames)]
+            if self.random_permute:
+                # want to permute here:
+                random.shuffle(frames)
+
+            # Get first frame object ids
+            visible_object_ids = []
+            loaded_segms = segment_loader.load(frames[0].frame_idx)
+            if isinstance(loaded_segms, LazySegments):
+                # LazySegments for SA1BRawDataset
+                visible_object_ids = list(loaded_segms.keys())
+            else:
+                for object_id, segment in segment_loader.load(
+                    frames[0].frame_idx
+                ).items():
+                    if segment.sum():
+                        visible_object_ids.append(object_id)
+
+            # First frame needs to have at least a target to track
+            if len(visible_object_ids) > 0:
+                break
+            if retry >= MAX_RETRIES - 1:
+                raise Exception("No visible objects")
+
+        object_ids = random.sample(
+            visible_object_ids,
+            min(len(visible_object_ids), self.max_num_objects),
+        )
+        return SampledFramesAndObjects(frames=frames, object_ids=object_ids)
 
 class EvalSampler(VOSSampler):
     """
